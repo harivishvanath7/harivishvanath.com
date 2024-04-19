c0290f5da284c348a10f81f245fd7f80c9f6e86b
HARI VISHVANATH S <117706207+harivishvanath7@users.noreply.github.com>
2024-04-19 23:28:54 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@
 // Scripts
 // 
 
-window.addEventListener('DOMContentLoaded', event => {
+window.addEventListener ('DOMContentLoaded', event => {
 
     // Activate Bootstrap scrollspy on the main nav element
     const sideNav = document.body.querySelector('#sideNav');
